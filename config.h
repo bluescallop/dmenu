6212d33337754c821c432cd4c96b1249a6de329f
bluescallop <scllp@protonmail.com>
2026-05-25 00:29:56 -0300

    Mudei o caminho das cores

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const char *fonts[] = {
 	"Hack Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-#include "/home/scallop/dmenu/colors-wal-dmenu.h"
+#include "/home/scallop/.cache/wal/colors-wal-dmenu.h"
 //static const char *colors[SchemeLast][2] = {
 //	/*     fg         bg       */
 //	[SchemeNorm] = { "#bbbbbb", "#222222" },
